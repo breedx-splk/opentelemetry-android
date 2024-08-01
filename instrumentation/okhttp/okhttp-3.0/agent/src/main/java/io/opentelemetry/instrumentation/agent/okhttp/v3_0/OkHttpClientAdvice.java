1cae23da92812583b34456aa70c44e935f7ac53f
Jason Plumb <jplumb@splunk.com>
2024-08-01 14:28:17 -0700

    null checks for safety

--- instrumentation/okhttp/okhttp-3.0/agent/src/main/java/io/opentelemetry/instrumentation/agent/okhttp/v3_0/OkHttpClientAdvice.java
+++ instrumentation/okhttp/okhttp-3.0/agent/src/main/java/io/opentelemetry/instrumentation/agent/okhttp/v3_0/OkHttpClientAdvice.java
@@ -5,6 +5,11 @@
 
 package io.opentelemetry.instrumentation.agent.okhttp.v3_0;
 
+import static io.opentelemetry.instrumentation.library.okhttp.v3_0.internal.OkHttp3Singletons.CALLBACK_CONTEXT_INTERCEPTOR;
+import static io.opentelemetry.instrumentation.library.okhttp.v3_0.internal.OkHttp3Singletons.CONNECTION_ERROR_INTERCEPTOR;
+import static io.opentelemetry.instrumentation.library.okhttp.v3_0.internal.OkHttp3Singletons.RESEND_COUNT_CONTEXT_INTERCEPTOR;
+import static io.opentelemetry.instrumentation.library.okhttp.v3_0.internal.OkHttp3Singletons.TRACING_INTERCEPTOR;
+
 import io.opentelemetry.instrumentation.library.okhttp.v3_0.internal.OkHttp3Singletons;
 import net.bytebuddy.asm.Advice;
 import okhttp3.OkHttpClient;
@@ -13,13 +18,15 @@ public class OkHttpClientAdvice {
 
     @Advice.OnMethodEnter
     public static void enter(@Advice.Argument(0) OkHttpClient.Builder builder) {
-        if (!builder.interceptors().contains(OkHttp3Singletons.CALLBACK_CONTEXT_INTERCEPTOR)) {
-            builder.interceptors().add(0, OkHttp3Singletons.CALLBACK_CONTEXT_INTERCEPTOR);
-            builder.interceptors().add(1, OkHttp3Singletons.RESEND_COUNT_CONTEXT_INTERCEPTOR);
-            builder.interceptors().add(2, OkHttp3Singletons.CONNECTION_ERROR_INTERCEPTOR);
+        if (!builder.interceptors().contains(CALLBACK_CONTEXT_INTERCEPTOR)) {
+            builder.interceptors().add(0, CALLBACK_CONTEXT_INTERCEPTOR);
+            builder.interceptors().add(1, RESEND_COUNT_CONTEXT_INTERCEPTOR);
+            if(CONNECTION_ERROR_INTERCEPTOR != null) {
+                builder.interceptors().add(2, CONNECTION_ERROR_INTERCEPTOR);
+            }
         }
-        if (!builder.networkInterceptors().contains(OkHttp3Singletons.TRACING_INTERCEPTOR)) {
-            builder.addNetworkInterceptor(OkHttp3Singletons.TRACING_INTERCEPTOR);
+        if (TRACING_INTERCEPTOR != null && !builder.networkInterceptors().contains(TRACING_INTERCEPTOR)) {
+            builder.addNetworkInterceptor(TRACING_INTERCEPTOR);
         }
     }
 }
